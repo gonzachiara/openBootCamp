857758fbde78a01abca625d0c8434855d8da4599
gonzachiara <97534308+gonzachiara@users.noreply.github.com>
2022-04-19 01:47:25 -0300

    Ejercicio 3, par e impar, números primos

---
+++ Par e impar, primos/numeroPrimo.js
@@ -0,0 +1,12 @@
+
+for (let z = 0; z < 11; z++) {
+    if (z <= 1) {
+        console.log(z + " Es numero primo");
+    }
+    for (let i = 2; i < z; i++) {
+        if (z % i == 0) {
+        console.log(z + " Es numero primo");
+        }
+    
+    }
+}
