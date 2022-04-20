3e044463fbbdbc6bcddb47b9f472273760904c2c
gonzachiara <97534308+gonzachiara@users.noreply.github.com>
2022-04-20 13:28:39 -0300

    Números aleatorios
    Se generan 10 números aleatorios que no sean mayores a 20

---
+++ Numero aleatorio x10/numerosAleatorios.js
@@ -0,0 +1,26 @@
+//En este ejercicio tendrás que generar 10 números aleatorios que estén comprendidos entre 0 y 20 haciendo uso de un bucle.
+
+let i = 0;
+if(i=1){
+console.log(parseInt(Math.random(i)*20));
+}if(i = 2){
+    console.log(parseInt(Math.random(i)*20));
+}if(i=3){
+    console.log(parseInt(Math.random(i)*20));
+}if(i = 4){
+    console.log(parseInt(Math.random(i)*20));
+}if(i=5){
+    console.log(parseInt(Math.random(i)*20));
+}if(i = 6){
+    console.log(parseInt(Math.random(i)*20));
+}if(i=7){
+    console.log(parseInt(Math.random(i)*20));
+}if(i = 8){
+    console.log(parseInt(Math.random(i)*20));
+}if(i=9){
+    console.log(parseInt(Math.random(i)*20));
+}if(i = 10){
+    console.log(parseInt(Math.random(i)*20));
+}
+
+// se llega al objetivo de una manera no convencional
