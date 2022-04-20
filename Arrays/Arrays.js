f803a289b16942eb3939aad3a977a4672a110b69
gonzachiara <97534308+gonzachiara@users.noreply.github.com>
2022-04-20 15:34:32 -0300

    Arrays
    Incrementacion de arrays

---
+++ Arrays/Arrays.js
@@ -0,0 +1,15 @@
+
+//Forma manual
+
+let i = [1,2,3,4,5,6,7,8,9];
+
+i.push(10,11,12,13,14,15,16,17,18,19,20);
+console.log(i)
+
+//Forma automatica
+let c = [];
+
+
+for( let i = 0; i < c.length; i++){
+    console.log(c);
+}
