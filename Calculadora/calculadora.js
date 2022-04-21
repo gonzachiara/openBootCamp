b9845ece63ce21732c9e985a916e5bdf1e87cec3
gonzachiara <97534308+gonzachiara@users.noreply.github.com>
2022-04-21 16:43:21 -0300

    Add files via upload

---
+++ Calculadora/calculadora.js
@@ -0,0 +1,22 @@
+function operaciones(op)
+{
+
+    var ops = {
+        sumar: function sumarNumeros(n1, n2) {
+            return (parseInt(n1) + parseInt(n2));
+        },
+
+        restar: function restarNumeros(n1, n2) {
+            return (parseInt(n1) - parseInt(n2));
+        },
+        
+        multiplicar: function multiplicarNumeros(n1, n2) {
+            return (parseInt(n1) * parseInt(n2));
+        },
+
+        dividir: function dividirNumeros(n1, n2) {
+            return (parseInt(n1) / parseInt(n2));
+        }
+
+
+    }};
