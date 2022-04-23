4137ed27b5c46ebbb37a6c0e3bfa36c839470ee4
gonzachiara <97534308+gonzachiara@users.noreply.github.com>
2022-04-23 00:26:54 -0300

    Update numeroPrimo.js

--- Par e impar, primos/numeroPrimo.js
+++ Par e impar, primos/numeroPrimo.js
@@ -1,12 +1,13 @@
-
+primos:
 for (let z = 0; z < 11; z++) {
     if (z <= 1) {
         console.log(z + " Es numero primo");
+        continue primos;
     }
     for (let i = 2; i < z; i++) {
         if (z % i == 0) {
         console.log(z + " Es numero primo");
         }
-    
+    continue primos;
     }
 }
