1f13cbf318f6683c16648db1400805e69b3e6c03
gonzachiara <97534308+gonzachiara@users.noreply.github.com>
2022-05-02 23:20:54 -0300

    Add files via upload

---
+++ Debug/Debug.js
@@ -0,0 +1,15 @@
+// Enunciado del ejercicio:
+
+// ¿Qué beneficios nos trae el debugging?
+
+// Indica cuál de los siguientes elementos son clave en el debugging.
+
+// Inspección de variables.
+
+// Análisis del código tras encontrar un error.
+
+// Ejecución paso a paso. X
+
+// La eficiencia de la ejecución del código. X
+
+// Las tres primeras respuestas son correctas
